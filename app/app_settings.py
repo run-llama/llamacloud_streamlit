9505d33c3ede6082aa432cd450a0d781cd85750a
Sourabh Desai <sourabhdesai@gmail.com>
2025-01-07 12:17:33 -0800

    update DEFAULT_LLAMA_CLOUD_API_URL

--- app/app_settings.py
+++ app/app_settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: SecretStr = Field(st.secrets.openai_key, description="OpenAI API Key")
-    DEFAULT_LLAMA_CLOUD_API_URL: str = Field("https://api.staging.llamaindex.ai", description="LlamaCloud API URL")
+    DEFAULT_LLAMA_CLOUD_API_URL: str = Field("https://api.cloud.llamaindex.ai", description="LlamaCloud API URL")
     DEFAULT_LLAMA_CLOUD_API_KEY: Optional[SecretStr] = Field(None, description="Default LlamaCloud API Key")
 
 settings = Settings()
